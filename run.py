7bc108005c115d07cb18e6f85960e8559e5bef44
Luise Higuera <luisehica@gmail.com>
2021-07-20 23:09:07 -0500

    Class Resources almost finished

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ from pytube import YouTube
 import ssl
 import os
 import subprocess
+
 import pandas as pd
+import numpy as np
 
 DESTINATION = './results/'
 
@@ -14,92 +16,126 @@ class Person:
     """
     Entity to a person voice data.
     """
-    def __init__(self, name):
+    def __init__(self, name, df):
         self.name = name
         self.dir = os.path.join(DESTINATION, name)
-        self.resources = []
+        self.df = df
+        
+        
+    @property
+    def filtered_df(self):
+        df = self.df
+        mask = df['Nombre']==self.name
+        filtered_df = df[mask]
+        return filtered_df
 
-    def add_resource(self, resource):
-        self.resources.append(resource)
+    @property
+    def resources(self):
+        return self.filtered_df['URL']
+
+    @property
+    def trims(self):
+        filtered_df = self.filtered_df
+        columns_trim = list(filtered_df.columns[3:])
+        filtered_df['trims'] = filtered_df[columns_trim].apply(lambda x: [i for i in x], axis=1)
+        return filtered_df[['URL', 'trims']]
 
 
 class Resources:
     """
     """
-    filename = ''
-    def __init__(self, url, list_chunk_times):
-        self.url = url
-        self.list_chunk_times = list_chunk_times
+    def __init__(self, Person):
+        self.person_name = Person.name
+        self.list_url = Person.resources                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+        self.list_trims = Person.trims
+        self.dir_folder = Person.dir
 
-    def filename(self, person_name, resource_name, duration):
-        self.filename = f"{person_name}-{resource_name}-{duration}.wav"
+    def create_filename(self, resource_name, duration):
+        """[summary]
 
-    def download_yt(self, dir):
-        yt = YouTube(self.url)
+        Args:
+            resource_name ([type]): [description]
+            duration ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return f"{self.person_name}-{resource_name}-{duration}.wav"
+
+    def get_duration(self, input_audio):
+        """Calculate duration audio in seconds
+
+        Args:
+            input_audio ([type]): Path of audiofile
+
+        Returns:
+            float: duration of audio in seconds
+        """
+
+        result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', input_audio], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        return float(result.stdout)
+
+    def download_yt(self, url):
+        """Download yt video using pytube
+
+        Args:
+            url ([type]): [description]
+        """
+        yt = YouTube(url)
         
         video = yt.streams.filter(only_audio=True).first()
 
-        video.download(dir)
+        video.download(self.dir_folder)
         
         default_filename = video.default_filename
 
-        duration = get_length(os.path.join(dir, default_filename))
-        filename(self, person_name, resource_name, duration)
+        duration = self.get_duration(os.path.join(dir, default_filename))
+        new_dir_filename = self.create_filename(default_filename[:7], duration)
 
-        # using pytube API
         stream = ffmpeg.input(os.path.join(dir, default_filename))
         stream = ffmpeg.output(stream,
-                                os.path.join(dir, self.filename))
+                                os.path.join(dir, new_dir_filename))
         ffmpeg.run(stream)
         os.remove(os.path.join(dir, default_filename))
+        return new_dir_filename
         
+    def trim_audio(self, new_dir_filename):
+        trims = self.list_trims
+        new_trims = []
+        # TODO: revisar que si quede una lista de listas con las parejas de cada recorte
+        for i in range(len(trims)/2):
+            new_trims.append([trims[i*2], trims[i*2+1]])
 
-    def trim_audio(self, start, end):
-        pass
+        for trim in new_trims:
+
+            audio_input = ffmpeg.input(new_dir_filename)
+            audio_cut = audio_input.audio.filter('atrim', start=trim[0], end=trim[1])
+            
+        audio_output = ffmpeg.output(audio_cut, os.path.join(self.dir_folder, 'out.wav'))
+        ffmpeg.run(audio_output)
+
     
 
-
-
-def get_length(input_audio):
-    """
-    Calculate duration audio in seconds
-    :param input_audio: The paht of audio
-    :return:
-    """
-    result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', input_audio], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return float(result.stdout)
-
-
 if __name__ == '__main__':
-
-    df = pd.read_csv("Audios.csv")
-
-    print(df)
-    os.exit
 
     # Create destination directory
     if not os.path.exists(DESTINATION):
         os.mkdir(DESTINATION)
 
-    persons = [Person('LUIS FERNANDO VELASCO')]
-    print(voices[0].dir)
-    os.exit_()
-    try:
-        ssl._create_default_https_context = ssl._create_unverified_context
+    df = pd.read_csv("Audios.csv")
+    names_congr = df['Nombre'].unique()
 
-        for person in persons:
+    for congr in names_congr:
+        congressman = Person(congr, df)
+        resources = Resources(congressman)
+        
+        try:
+            ssl._create_default_https_context = ssl._create_unverified_context
 
-            dir = os.path.join(DESTINATION, voice.name) 
+            for url in resources.list_url:
+                new_dir_filename = Resources.download_yt(url)
+                Resources.trim_audio(new_dir_filename)
 
-            res = Resources(url, list_chunk_times)
-
-            # trim audio
-            audio_input = ffmpeg.input(os.path.join(dir, new_filename))
-            #audio_cut = audio_input.audio.filter('atrim', duration=5)
-            audio_cut = audio_input.audio.filter('atrim', start=30, end=40)
-            audio_output = ffmpeg.output(audio_cut, os.path.join(dir, 'out.wav'))
-            ffmpeg.run(audio_output)
-
-    except Exception as ex:
-        print("Not could download audio file")
-        print(ex)
+        except Exception as ex:
+            print("Not could download audio file")
+            print(ex)
